11057fb2834ac2b4becc9302d2740169ef5a735c
darkfusion90 <gauravsharmaa341@gmail.com>
2020-05-08 20:52:40 +0530

    Added toString implementation

--- lib/models/Image.dart
+++ lib/models/Image.dart
@@ -2,18 +2,23 @@ class Image {
   final String id;
   final String altDesc;
   final Map<String, dynamic> urls;
-  
+
   Image({this.id, this.urls, this.altDesc});
-  
+
   factory Image.fromJson(Map<String, dynamic> json) {
     return Image(
-      id: json['id'], urls: json['urls'], altDesc: json['alt_description']);
+        id: json['id'], urls: json['urls'], altDesc: json['alt_description']);
   }
-  
+
   static List<Image> fromJsonList(List<Map<String, dynamic>> jsonList) {
     return List<Image>.generate(
       jsonList.length,
-        (index) => Image.fromJson(jsonList[index]),
+      (index) => Image.fromJson(jsonList[index]),
     );
+  }
+
+  @override
+  String toString() {
+    return "{'id': $id, 'altDesc': $altDesc}\n";
   }
 }
