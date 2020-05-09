d9030e9b8d45d06beee7ee1f819fc32055ef92ee
darkfusion90 <gauravsharmaa341@gmail.com>
2020-05-09 17:50:53 +0530

    Separated Image building to a separate widget

--- lib/widgets/SearchResults.dart
+++ lib/widgets/SearchResults.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:searchimages/models/main.dart' as models;
 import 'package:searchimages/utils/api.dart' as api;
-import 'package:searchimages/controllers/favorites.dart' as favorites;
+import 'image_container/ImageContainer.dart' show ImageContainer;
 
 class SearchResults extends StatefulWidget {
   final String searchQuery;
@@ -15,117 +15,6 @@ class SearchResults extends StatefulWidget {
 
 class SearchResultsState extends State<SearchResults> {
   Future<List<models.Image>> _futureImageList;
-  List<models.Favorite> _favoritesList = new List<models.Favorite>();
-
-  void _addToFavorites(models.Image image) async {
-    await favorites.createFavorite(image);
-    _refreshFavorites();
-  }
-
-  void _removeFromFavorites(models.Image image) async {
-    await favorites.deleteFavorite(image);
-    _refreshFavorites();
-  }
-
-  bool _isAddedToFavorites(models.Image image) {
-    print('check for ${image.id} in $_favoritesList');
-    models.Favorite result = _favoritesList.singleWhere(
-      (models.Favorite fav) => fav.imageId.compareTo(image.id) == 0,
-      orElse: () => null,
-    );
-    return result != null;
-  }
-
-  Widget _buildImageActions(models.Image image) {
-    bool isAlreadyFavorite = _isAddedToFavorites(image);
-
-    Icon getFavoriteIcon() {
-      return Icon(isAlreadyFavorite ? Icons.favorite : Icons.favorite_border);
-    }
-
-    List<Widget> getActions() {
-      return [
-        Tooltip(
-          child: IconButton(
-            padding: EdgeInsets.all(0),
-            icon: getFavoriteIcon(),
-            onPressed: () => isAlreadyFavorite
-                ? _removeFromFavorites(image)
-                : _addToFavorites(image),
-          ),
-          message: 'Add To Favorites',
-        )
-      ];
-    }
-
-    return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: getActions(),
-      ),
-    );
-  }
-
-  Widget _buildGridTile(models.Image image) {
-    return Container(
-      decoration: BoxDecoration(color: Theme.of(context).primaryColor),
-      margin: EdgeInsets.all(8.0),
-      child: Column(
-        children: <Widget>[
-          Image(image: NetworkImage(image.urls['thumb'])),
-          _buildImageActions(image)
-        ],
-      ),
-    );
-  }
-
-  Widget _buildEmptySearchResults() {
-    return Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Icon(
-            Icons.sentiment_dissatisfied,
-            size: 64.0,
-            color: Colors.grey,
-          ),
-          Text(
-            'No results',
-            style: TextStyle(fontSize: 32.0, color: Colors.grey),
-          ),
-        ]);
-  }
-
-  Widget _buildData(data) {
-    if (data.isEmpty) {
-      return _buildEmptySearchResults();
-    }
-
-    final int itemCountWithExtraForSpinner = data.length + 1;
-    return StaggeredGridView.countBuilder(
-      crossAxisCount: 2,
-      itemCount: itemCountWithExtraForSpinner,
-      itemBuilder: (context, index) {
-        return index == data.length
-            ? Container(
-                child: Center(child: CircularProgressIndicator()),
-                padding: EdgeInsets.all(16.0),
-              )
-            : _buildGridTile(data[index]);
-      },
-      staggeredTileBuilder: (index) =>
-          new StaggeredTile.fit(index == data.length ? 2 : 1),
-    );
-  }
-
-  void _refreshFavorites() async {
-    final List<models.Favorite> newFavorites =
-        await favorites.getAllFavorites();
-
-    setState(() {
-      _favoritesList = List<models.Favorite>.from(newFavorites);
-    });
-  }
 
   @override
   void didUpdateWidget(SearchResults oldWidget) {
@@ -145,6 +34,65 @@ class SearchResultsState extends State<SearchResults> {
         _futureImageList = api.fetchImages(widget.searchQuery);
       });
     }
+  }
+
+  bool _isSearchQueryEmpty() {
+    final String searchQuery = widget.searchQuery ?? '';
+    return searchQuery.isEmpty;
+  }
+
+  Widget _buildEmptySearchResults() {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.sentiment_dissatisfied,
+            size: 64.0,
+            color: Colors.grey,
+          ),
+          Text(
+            'No results',
+            style: TextStyle(fontSize: 32.0, color: Colors.grey),
+          ),
+        ]);
+  }
+
+  Widget _buildLoadingScreen() {
+    return Container(
+      child: Center(child: CircularProgressIndicator()),
+      padding: EdgeInsets.all(16.0),
+    );
+  }
+
+  Widget _buildInitialState() {
+    return Container(
+      child: Center(child: Text('Type something in the search bar')),
+      padding: EdgeInsets.all(16.0),
+    );
+  }
+
+  Widget _buildData(data) {
+    if (_isSearchQueryEmpty()) {
+      return _buildInitialState();
+    }
+
+    if (data.isEmpty) {
+      return _buildEmptySearchResults();
+    }
+
+    final int itemCountWithExtraForSpinner = data.length + 1;
+    return StaggeredGridView.countBuilder(
+      crossAxisCount: 2,
+      itemCount: itemCountWithExtraForSpinner,
+      itemBuilder: (context, index) {
+        return index == data.length
+            ? _buildLoadingScreen()
+            : ImageContainer(data[index]);
+      },
+      staggeredTileBuilder: (index) =>
+          new StaggeredTile.fit(index == data.length ? 2 : 1),
+    );
   }
 
   @override
