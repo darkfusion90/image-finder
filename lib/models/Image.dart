91298e3d65583f353dc4912ac4b15222c0abfbb6
darkfusion90 <gauravsharmaa341@gmail.com>
2020-05-10 09:41:40 +0530

    All data members made compulsory(had positional args earlier) Added getters for width and height using optional aspect ratio from widths specified in IMAGE_WIDTHS

--- lib/models/Image.dart
+++ lib/models/Image.dart
@@ -1,15 +1,31 @@
 enum ImageUrlTypes { RAW, FULL, REGULAR, SMALL, THUMB }
 
+const Map<ImageUrlTypes, int> IMAGE_WIDTHS = {
+  ImageUrlTypes.REGULAR: 1080,
+  ImageUrlTypes.SMALL: 400,
+  ImageUrlTypes.THUMB: 200,
+};
+
 class Image {
   final String id;
   final String altDesc;
+  final int width;
+  final int height;
   final Map<String, dynamic> urls;
+  double aspectRatio;
 
-  Image({this.id, this.urls, this.altDesc});
+  Image(this.id, this.width, this.height, this.urls, this.altDesc) {
+    this.aspectRatio = this.width / this.height;
+  }
 
   factory Image.fromJson(Map<String, dynamic> json) {
     return Image(
-        id: json['id'], urls: json['urls'], altDesc: json['alt_description']);
+      json['id'],
+      json['width'],
+      json['height'],
+      json['urls'],
+      json['alt_description'],
+    );
   }
 
   static List<Image> fromJsonList(List<Map<String, dynamic>> jsonList) {
@@ -34,6 +50,23 @@ class Image {
       default:
         throw new Exception('Invalid urlType: $urlType');
     }
+  }
+
+  int getWidth({ImageUrlTypes urlType}) {
+    return IMAGE_WIDTHS.containsKey(urlType)
+        ? IMAGE_WIDTHS[urlType]
+        : this.width;
+  }
+
+  int getHeight({ImageUrlTypes urlType}) {
+    return IMAGE_WIDTHS.containsKey(urlType)
+        ? this._calculateImageHeight(IMAGE_WIDTHS[urlType])
+        : this.height;
+  }
+
+  int _calculateImageHeight(int width) {
+    //Operator ~/ returns the int value casted from the expression
+    return width ~/ aspectRatio;
   }
 
   @override
