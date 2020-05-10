2a130640381b78dbb577f1190bdba91db9043587
darkfusion90 <gauravsharmaa341@gmail.com>
2020-05-10 09:42:26 +0530

    Added placeholder while the image is being loaded

--- lib/widgets/image_container/ImageContainer.dart
+++ lib/widgets/image_container/ImageContainer.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
 import 'package:searchimages/models/main.dart' as models;
+import 'package:searchimages/models/Image.dart' show ImageUrlTypes;
 import 'ImageActions.dart' show ImageActions;
 
 class ImageContainer extends StatelessWidget {
@@ -7,16 +10,29 @@ class ImageContainer extends StatelessWidget {
 
   ImageContainer(this._image);
 
+  Widget _buildImageLoadingPlaceholder(BuildContext context, String url) {
+    return Container(
+      child: Text(_image.altDesc),
+      color: Colors.deepOrange,
+      width: _image.getWidth(urlType: ImageUrlTypes.THUMB).toDouble(),
+      height: _image.getHeight(urlType: ImageUrlTypes.THUMB).toDouble(),
+    );
+  }
+
+  Widget _buildImage() {
+    return CachedNetworkImage(
+      imageUrl: _image.getUrl(ImageUrlTypes.THUMB),
+      placeholder: _buildImageLoadingPlaceholder,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(color: Theme.of(context).primaryColor),
       margin: EdgeInsets.all(8.0),
       child: Column(
-        children: <Widget>[
-          Image(image: NetworkImage(this._image.urls['thumb'])),
-          ImageActions(this._image)
-        ],
+        children: <Widget>[_buildImage(), ImageActions(this._image)],
       ),
     );
   }
