28fec35db5fc839fc3c6993ef97d669ca6b84713
darkfusion90 <gauravsharmaa341@gmail.com>
2020-05-11 05:12:38 +0530

    Represents one grid tile in ImageGridView

---
+++ lib/widgets/image_grid_tile/ImageGridTile.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'package:searchimages/models/main.dart' as models;
+import 'package:searchimages/widgets/ImageContainer.dart';
+import 'package:searchimages/widgets/ImageDetails.dart';
+import 'ImageActions.dart' show ImageActions;
+
+class ImageGridTile extends StatelessWidget {
+  final models.Image _image;
+
+  ImageGridTile(this._image);
+  
+  Widget _buildImage(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (_) => ImageDetails(_image)));
+      },
+      child: ImageContainer(_image)
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[_buildImage(context), ImageActions(this._image)],
+      ),
+    );
+  }
+}
