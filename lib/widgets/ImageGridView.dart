2295db3106610e4617c010bd3fc8e367ac0a3e4f
darkfusion90 <gauravsharmaa341@gmail.com>
2020-05-11 05:16:28 +0530

    ImageActions and ImageGridTile moved out of package image_grid_tile

--- lib/widgets/ImageGridView.dart
+++ lib/widgets/ImageGridView.dart
@@ -1,7 +1,7 @@
 import'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
-import 'image_grid_tile/ImageGridTile.dart' show ImageGridTile;
+import 'ImageGridTile.dart' show ImageGridTile;
 import 'package:searchimages/models/main.dart' as models;
 
 class ImageGridView extends StatefulWidget {
