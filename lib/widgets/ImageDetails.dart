0eb4a7d7816ed2956ed4c2a4e27f96482027db08
darkfusion90 <gauravsharmaa341@gmail.com>
2020-05-11 05:36:27 +0530

    Hides device status bar (the one that shows battery, network bars, notification icons, etc) on initState and onTap in body. Shows(or restores) the status bar when state disposed

--- lib/widgets/ImageDetails.dart
+++ lib/widgets/ImageDetails.dart
@@ -1,19 +1,61 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:searchimages/models/main.dart' as models;
 import 'package:searchimages/models/Image.dart' show ImageUrlTypes;
 import 'package:searchimages/widgets/ImageContainer.dart';
 
-class ImageDetails extends StatelessWidget {
+class ImageDetails extends StatefulWidget {
   final models.Image _image;
 
   ImageDetails(this._image);
 
   @override
+  State<StatefulWidget> createState() => _ImageDetailsState();
+}
+
+class _ImageDetailsState extends State<ImageDetails> {
+  @override
+  void initState() {
+    super.initState();
+    _hideDeviceStatusBar();
+  }
+
+  @override
+  void dispose() {
+    _showDeviceStatusBar();
+    super.dispose();
+  }
+
+  void _hideDeviceStatusBar() {
+    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
+  }
+
+  void _showDeviceStatusBar() {
+    SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      child: ImageContainer(_image, imageDisplaySize: ImageUrlTypes.SMALL),
-      onTap: () => Navigator.pop(context),
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.black),
+      body: GestureDetector(
+        child: _buildContent(context),
+        onTap: _hideDeviceStatusBar,
+      ),
+    );
+  }
+
+  Widget _buildContent(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: Center(
+        child: ImageContainer(
+          widget._image,
+          imageDisplaySize: ImageUrlTypes.SMALL,
+        ),
+      ),
     );
   }
 }
