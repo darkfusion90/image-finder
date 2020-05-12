e4de8ddfd5b97149315255091214c9f6911c2b13
darkfusion90 <gauravsharmaa341@gmail.com>
2020-05-12 09:33:55 +0530

    Added all fields available in AppBar

--- lib/widgets/navs/appbar/BaseAppBar.dart
+++ lib/widgets/navs/appbar/BaseAppBar.dart
@@ -4,16 +4,69 @@ import 'package:flutter/material.dart';
 class BaseAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   final Size preferredSize;
-
   final Widget title;
+  final Color backgroundColor;
   final List<Widget> actions;
+  final Widget leading;
+  final bool automaticallyImplyLeading;
+  final Widget flexibleSpace;
+  final PreferredSizeWidget bottom;
+  final double elevation;
+  final ShapeBorder shape;
+  final Brightness brightness;
+  final IconThemeData iconTheme;
+  final IconThemeData actionsIconTheme;
+  final TextTheme textTheme;
+  final bool primary;
+  final bool centerTitle;
+  final double titleSpacing;
+  final double toolbarOpacity;
+  final double bottomOpacity;
 
-  BaseAppBar({Key key, this.title, this.actions})
-      : preferredSize = Size.fromHeight(kToolbarHeight),
+  BaseAppBar({
+    Key key,
+    this.leading,
+    this.automaticallyImplyLeading = true,
+    this.title,
+    this.actions,
+    this.flexibleSpace,
+    this.bottom,
+    this.elevation,
+    this.shape,
+    this.backgroundColor,
+    this.brightness,
+    this.iconTheme,
+    this.actionsIconTheme,
+    this.textTheme,
+    this.primary = true,
+    this.centerTitle,
+    this.titleSpacing = NavigationToolbar.kMiddleSpacing,
+    this.toolbarOpacity = 1.0,
+    this.bottomOpacity = 1.0,
+  })  : preferredSize = Size.fromHeight(kToolbarHeight),
         super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(title: this.title, actions: this.actions);
+    return AppBar(
+      actions: this.actions,
+      actionsIconTheme: this.actionsIconTheme,
+      automaticallyImplyLeading: this.automaticallyImplyLeading,
+      backgroundColor: this.backgroundColor,
+      bottom: this.bottom,
+      bottomOpacity: this.bottomOpacity,
+      brightness: this.brightness,
+      centerTitle: this.centerTitle,
+      elevation: this.elevation,
+      flexibleSpace: this.flexibleSpace,
+      iconTheme: this.iconTheme,
+      leading: this.leading,
+      primary: this.primary,
+      shape: this.shape,
+      textTheme: this.textTheme,
+      titleSpacing: this.titleSpacing,
+      toolbarOpacity: this.toolbarOpacity,
+      title: this.title,
+    );
   }
 }
