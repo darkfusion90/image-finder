8b71894737c31a1c5c260d3091a03aa22d41412a
darkfusion90 <gauravsharmaa341@gmail.com>
2020-05-13 18:16:51 +0530

    Colors changed: bg is white and added shadow to tile

--- lib/widgets/home_page/widgets/image_grid_view/ImageGridTile.dart
+++ lib/widgets/home_page/widgets/image_grid_view/ImageGridTile.dart
@@ -20,7 +20,10 @@ class ImageGridTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5,offset: Offset(0.5, 2))],
+      ),
       margin: EdgeInsets.all(8.0),
       child: Column(
         children: <Widget>[_buildImage(context), ImageActions(this._image)],
