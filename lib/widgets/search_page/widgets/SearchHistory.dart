cc5c774334bd9b97059ab8c5bd4f71dbea9710fb
darkfusion90 <gauravsharmaa341@gmail.com>
2020-05-17 16:43:38 +0530

    Requesting focus on tapping any item since not doing so will keep the keyboard hovering on the screen for no reason (the keyboard could be brought to screen by the TextField in the AppBar)

--- lib/widgets/search_page/widgets/SearchHistory.dart
+++ lib/widgets/search_page/widgets/SearchHistory.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:searchimages/database/controllers/searchQueries.dart'
     as searchQueries;
 import 'package:searchimages/database/models/SearchQuery.dart';
+import 'package:searchimages/utils/widget-based.dart';
 
 typedef CompareToFunction = int Function(SearchQuery, SearchQuery);
 
@@ -53,12 +54,22 @@ class _SearchHistoryState extends State<SearchHistory> {
     return ListTile(
       contentPadding: EdgeInsets.only(left: 24.0, right: 12.0),
       title: Text(searchQuery.query),
-      onTap: () => widget.onSearchHistoryItemTapped(searchQuery.query),
+      onTap: () => _handleOnSearchHistoryItemTap(context, searchQuery),
       trailing: IconButton(
         icon: Icon(Icons.delete_outline),
         onPressed: () => this._deleteSearchQuery(searchQuery),
       ),
     );
+  }
+
+  void _handleOnSearchHistoryItemTap(
+    BuildContext context,
+    SearchQuery searchQuery,
+  ) {
+    // Requesting focus here to remove the focus
+    // from the TextField in the AppBar which further removes the keyboard from the screen
+    requestFocus(context);
+    widget.onSearchHistoryItemTapped(searchQuery.query);
   }
 
   void _deleteSearchQuery(SearchQuery searchQuery) {
